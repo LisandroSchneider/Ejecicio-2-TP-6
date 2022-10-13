35ced513f6e88cb42959ba7b0df1b4d71d053519
lisandroschneider <lisandroschneider7@gmail.com>
2022-10-13 18:22:06 -0300

    subir archivo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio_n_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("Escriba su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("¡Bienvenido {0}!",nombre);
+
+            Console.WriteLine("Presione enter para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
